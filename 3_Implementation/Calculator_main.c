ffcddc73d0c628f57ad1a1d91ba0cf9beebbb6f3
Narra Revathi <80764179+260980@users.noreply.github.com>
2021-04-16 09:25:59 +0530

    Update Calculator_main.c

--- 3_Implementation/Calculator_main.c
+++ 3_Implementation/Calculator_main.c
@@ -5,10 +5,9 @@
 
 static result res={0};
 static calculator number1;
-static calculator number2;
 void calc()
 {
-    int choice,lnum1,lnum2;
+    int choice,num1,num2;
     printf("Enter your choice\n");
     printf("1.ADDITION\n  2.SUBTRACTION\n   3.MULTIPLICATION\n   4.DIVISION\n    5.POWER\n     6.SQUARE ROOT\n");
     scanf("%d",&choice);
@@ -20,45 +19,45 @@ void calc()
     else if(choice==6)
     {
         printf("Enter the value\n");
-        scanf("%d",&lnum1);
+        scanf("%d",&num1);
     }
     else{
         printf("Enter the values\n");
-        scanf("%d %d",&lnum1,&lnum2);
-        number1.num1=lnum1;
-        number2.num2=lnum2;
+        scanf("%d %d",&num1,&num2);
+        number1.num1=num1;
+        number1.num2=num2;
     }
     switch(choice)
     {
         case 1:
-        calc_add(number2,number2,&res);
+        calc_add(number1,&res);
         printf("Addition is %.2f",res.final_ans);
         break;
 
         case 2:
-        calc_sub(number1,number2,&res);
+        calc_sub(number1,&res);
         printf("Subtraction is %.2f",res.final_ans);
         break;
 
         case 3:
-        calc_mul(number1,number2,&res);
+        calc_mul(number1,&res);
         printf("Multiplication is %.2f",res.final_ans);
         break;
 
         case 4:
-        calc_div(number1,number2,&res);
+        calc_div(number1,&res);
         printf("Division is %.2f",res.final_ans);
         break;
 
         case 5:
-        calc_power(number1,number2,&res);
+        calc_power(number1,&res);
         printf("Power is %.2f",res.final_ans);
         break;
 
-        case 6:
+       /* case 6:
         calc_square(number1,&res);
         printf("Square root is %.2f",res.final_ans);
-        break;
+        break; */
 
         default:
         exit(0);
