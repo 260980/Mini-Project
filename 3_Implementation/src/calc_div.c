e66322d044e0dc02876bd7d12ea313bcd67699f9
Narra Revathi <80764179+260980@users.noreply.github.com>
2021-04-16 09:30:30 +0530

    Update calc_div.c

--- 3_Implementation/src/calc_div.c
+++ 3_Implementation/src/calc_div.c
@@ -10,16 +10,16 @@
  * @return verify 
  */
 
-verify calc_div(calculator number1,calculator number2,result* res)
+verify calc_div(calculator number1,result* res)
 {
-    if(number1.num1==0 || number2.num2==0)
+    if(number1.num1==0 || number1.num2==0)
     {
        
         return ERROR_DIV_ZERO;
     }
     else
     {
-    res->final_ans=number1.num1/number2.num2;
+    res->final_ans=number1.num1/number1.num2;
     return SUCCESS;
     }
 }
